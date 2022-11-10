b3b68b9b57db609e1b41798b7b85fbadd8436d3f
Carol Bradsen <carolbradsen@gmail.com>
2022-11-09 20:15:32 -0700

    Add array

---
+++ friends.js
@@ -0,0 +1,11 @@
+var arrays = ["sean", "Jenna", "Tracey", "Dalton", "Leigh"]
+console.log(arrays[2]);
+// this writes code to print third element.
+var arrays = ["sean", "Jenna", "Tracey", "Dalton", "Leigh"]
+console.log(arrays[4]);
+// this writes code to print the last (5th) element, which is the 4th index position.
+var arrays = ["sean", "Jenna", "Tracey", "Dalton", "Leigh"]
+console.log(arrays[0]);
+// this writes code to print the first element, index position, 0.
+
+
